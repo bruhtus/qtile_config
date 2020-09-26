a667e5f9b062e1f74cd252ec7d11211981e76bf1
bruhtus <hibruhtus@gmail.com>
2020-09-26 13:38:51 +0700

    Add multi monitor setting

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ import re
 import socket
 import subprocess
 
+from Xlib import display as xdisplay
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget, hook
@@ -154,6 +155,32 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
+def get_num_monitors():
+    num_monitors = 0
+    try:
+        display = xdisplay.Display()
+        screen = display.screen()
+        resources = screen.root.xrandr_get_screen_resources()
+
+        for output in resources.outputs:
+            monitor = display.xrandr_get_output_info(output, resources.config_timestamp)
+            preferred = False
+            if hasattr(monitor, "preferred"):
+                preferred = monitor.preferred
+            elif hasattr(monitor, "num_preferred"):
+                preferred = monitor.num_preferred
+            if preferred:
+                num_monitors += 1
+
+    except Exception as e:
+        # always setup at least one monitor
+        return 1
+
+    else:
+        return num_monitors
+
+num_monitors = get_num_monitors()
+
 screens = [
     Screen(
         top=bar.Bar(
@@ -199,6 +226,10 @@ screens = [
         ),
     ),
 ]
+
+if num_monitors > 1:
+    for m in range(num_monitors - 1):
+        screens
 
 # Drag floating layouts.
 mouse = [
